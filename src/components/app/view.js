045b98f3a786c7dc77e6b94b6f149e55feeda217
Connor Smallman <connor.smallman@bedegaming.com>
2016-02-08 16:28:52 +0000

    Check for local storage before ajax request

--- src/components/app/view.js
+++ src/components/app/view.js
@@ -26,22 +26,32 @@ export default class extends React.Component{
 
   getWeather() {
     this.getLocation().then(location => {
-      let url = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${location.latitude}&lon=${location.longitude}&cnt=5&mode=json&appid=${apiKey}&units=metric`;
-      
-      qwest.get(url, null, {cache: true}).then((xhr, response) => {
-        let week = response.list.map(day => {
-          day.name = getDayName(day.dt);
-          return day;
-        });
+      let weather = localStorage.getItem('weather');
 
-        this.setState({ 
-          city: response.city.name, 
-          country: response.country, 
-          week,
-        }, () => {
-          this.props.history.pushState(null, 'Today');
+      if(!weather) {
+        let url = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${location.latitude}&lon=${location.longitude}&cnt=5&mode=json&appid=${apiKey}&units=metric`;
+
+        return qwest.get(url, null, {cache: true}).then((xhr, response) => {
+          response.list.map(day => {
+            day.name = getDayName(day.dt);
+            return day;
+          });
+
+          response.date = new Date();
+
+          localStorage.setItem('weather', response);
         });
-      });
+      } else {
+        return weather;
+      }
+    }).then(function(weather) {
+      this.setState({ 
+            city: weather.city.name, 
+            country: weather.country, 
+            weather: weather.list,
+          }, () => {
+            this.props.history.pushState(null, 'Today');
+          });
     });
   }
 
